5f8497fbfb386ada6eae05174b46064d648bb927
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-01-27 13:57:05 +0000

    Add code to load shared library

--- src/amulet/faster/__init__.py
+++ src/amulet/faster/__init__.py
@@ -12,7 +12,21 @@ _logging.basicConfig(level=_logging.INFO, format="%(levelname)s - %(message)s")
 
 
 def _init() -> None:
+    import os
     import sys
+    import ctypes
+
+    if sys.platform == "win32":
+        lib_path = os.path.join(os.path.dirname(__file__), "faster.dll")
+    elif sys.platform == "darwin":
+        lib_path = os.path.join(os.path.dirname(__file__), "libfaster.dylib")
+    elif sys.platform == "linux":
+        lib_path = os.path.join(os.path.dirname(__file__), "libfaster.so")
+    else:
+        raise RuntimeError(f"Unsupported platform {sys.platform}")
+
+    # Load the shared library
+    ctypes.cdll.LoadLibrary(lib_path)
 
     from ._faster import init
 
